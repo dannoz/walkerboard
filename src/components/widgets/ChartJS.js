88981e28f664f1e222e0f809c766649352bb6530
bpneal <ben@bneal.co.uk>
2015-09-22 11:41:51 +0100

    Add Horizonal Bar

--- src/components/widgets/ChartJS.js
+++ src/components/widgets/ChartJS.js
@@ -1,6 +1,11 @@
 import React from "react";
 import PanelError from "../PanelError";
 import ChartJs from "react-chartjs";
+import { createClass } from "react-chartjs/lib/core";
+
+require("../../lib/HorizontalBar.js");
+
+ChartJs.HorizontalBar = createClass("HorizontalBar", ["getBarsAtEvent"]);
 
 const defaultOptions = {
     multiTooltipTemplate: "<%= datasetLabel %> - <%= value %>"
